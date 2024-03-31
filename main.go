2cde692e77230fceea0236ca6752a8a5f4f7e8e7
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-03-31 12:44:45 -0300

    Deixando com estrutra melhor

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	RoutesPing "github.com/your/repo/Routes"
 )
 
 func main() {
 	app := fiber.New()
-	app.Get("/Ping", Ping)
+	app.Get("/Ping", RoutesPing.Ping)
 	app.Listen(":3000")
-}
-
-func Ping(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Pong",
-	})
 }
